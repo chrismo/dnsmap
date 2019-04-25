a1bf779bc1c6402f5d2726277f4f91fcb9340ac9
chrismo <chrismo@clabs.org>
2019-04-25 00:16:18 -0500

    push more into digger, keeping output separate

--- dnsmap.rb
+++ dnsmap.rb
@@ -21,7 +21,7 @@ class Servers
 
     download = open(url)
     CSV.new(download, headers: true).
-      select { |r| r["reliability"].to_f > 0.9 }.
+      select { |r| r["reliability"].to_f >= 0.96 }.
       select { |r| r["ip"] =~ /\d+\.\d+\.\d+\.\d+/ }
   end
 
@@ -39,63 +39,80 @@ class Servers
   end
 end
 
-def group_by_domains(output)
-  return output if output =~ /^DigError/
-
-  output.
-    uniq.
-    sort.
-    map { |ln| ln.downcase.split(/\./) }.
-    group_by { |ary| ary[1..2].join(".") }.
-    map { |domain, servers| [domain, servers.map(&:first)] }.to_h
-end
-
-def registrar_nameservers(domain)
-  # TODO: dnsruby has a Recursor class which is really complicated internally.
-  # TODO: \ Trying to re-use it here looks like a lot of work. Cool to be able
-  # TODO: \ to do, but, not worth my time right now.
-
-  registrar_nameservers = `whois #{domain}`.scan(/Name Server: (\S+).*/).flatten
-  result = group_by_domains(registrar_nameservers)
-
-  puts "Registrar Nameservers:"
-  puts "======================"
-  puts result
-  puts
-
-  result
-end
 
 class Digger
   class DigError < StandardError; end
 
-  def self.dig_ns_records(domain, ip)
+  attr_reader :result
+
+  def initialize(dns_server_ip, domain)
+    @dns_server_ip = dns_server_ip
+    @domain = domain
+  end
+
+  def dig_ns_records
+    @result = group_by_domains(dig).tap { |r| yield r }
+  end
+
+  def authoritative_ns_records
+    registrar_name_servers
+  end
+
+  private
+
+  def registrar_name_servers
+    # TODO: dnsruby has a Recursor class which is really complicated internally.
+    # TODO: \ Trying to re-use it here looks like a lot of work. Cool to be able
+    # TODO: \ to do, but, not worth my time right now.
+
+    registrar_nameservers = `whois #{@domain}`.scan(/Name Server: (\S+).*/).flatten
+    @result = group_by_domains(registrar_nameservers)
+  end
+
+  def dig
     # `dig @#{ip} ns #{domain} +short +time=1`.split("\n")
-    opts = {nameserver: ip, do_caching: false, query_timeout: 1}
-    Dnsruby::Resolver.new(opts).query(domain, 'ns').answer.map(&:domainname).map(&:to_s)
+    opts = {nameserver: @dns_server_ip, do_caching: false, query_timeout: 1}
+    Dnsruby::Resolver.new(opts).query(@domain, 'ns').answer.map(&:domainname).map(&:to_s)
   rescue => e
     "DigError: #{e.class.to_s}: #{e.message}"
+  end
+
+  def group_by_domains(output)
+    return output if output =~ /^DigError/
+
+    output.
+      uniq.
+      sort.
+      map { |ln| ln.downcase.split(/\./) }.
+      group_by { |ary| ary[1..2].join(".") }.
+      map { |domain, servers| [domain, servers.map(&:first)] }.to_h
   end
 end
 
 def dns_results(domain, geo_area, authoritative)
+  puts "Authoritative"
+  puts "============="
+  puts authoritative
+  puts
+
   geo_area = ["global", "us"].include?(geo_area) ? geo_area : "global"
   ips = [{country_id: "US", name: "google", ip: "8.8.8.8", },
          {country_id: "US", name: "cloudflare", ip: "1.1.1.1", }] +
     Servers.send("latest_reliable_#{geo_area}_servers")
 
+  # TODO: threaded lookups
   queue = Queue.new
   ips.each { |ip| queue.push(ip) }
 
   puts "Checking #{ips.length} servers"
   results = ips.map do |r|
-    r[:result] = group_by_domains(Digger.dig_ns_records(domain, r[:ip])).tap do |server_result|
+    r[:result] = Digger.new(r[:ip], domain).dig_ns_records do |server_result|
       if server_result =~ /^DigError/
         print "e"
       elsif server_result != authoritative
-        print("x")
+        print "x"
       else
-        print(".")
+        print "."
       end
     end
     r
@@ -114,6 +131,8 @@ if domain.nil?
   exit(1)
 end
 
+# TODO: options: filter out matches in table, and reliability factor
+
 geo_area = ARGV[1]
-authoritative = registrar_nameservers domain
+authoritative = Digger.new(nil, domain).authoritative_ns_records
 dns_results domain, geo_area, authoritative
